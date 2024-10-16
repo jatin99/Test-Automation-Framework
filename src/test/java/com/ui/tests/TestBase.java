ae0ad6216a6a4c9c24968230815fe0b1a2528318
Jatin <jatinvsharma@gmail.com>
2024-10-17 00:00:15 +0530

    uncommented the AfterMethod in TestBase

--- src/test/java/com/ui/tests/TestBase.java
+++ src/test/java/com/ui/tests/TestBase.java
@@ -43,15 +43,15 @@ public class TestBase {
 	public BrowserUtility getInstance() {
 		return homePage;
 	}
-//
-//	@AfterMethod(description = "Tear Down the browser")
-//	public void tearDown() {
-//
-//		if (isLambdaTest) {
-//			LambdaTestUtlity.quitSession(); // quit or close the browsersession on LT
-//		} else {
-//			homePage.quit(); // local
-//		}
-//	}
+
+	@AfterMethod(description = "Tear Down the browser")
+	public void tearDown() {
+
+		if (isLambdaTest) {
+			LambdaTestUtlity.quitSession(); // quit or close the browsersession on LT
+		} else {
+			homePage.quit(); // local
+		}
+	}
 
 }
