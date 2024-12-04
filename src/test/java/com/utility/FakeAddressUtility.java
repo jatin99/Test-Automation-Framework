8c4028729fb9bd2ceee5115f2a5aed5cfc679ed2
Jatin Shharma <jatinvsharma@gmail.com>
2024-12-04 21:50:11 +0530

    Update FakeAddressUtility.java

--- src/test/java/com/utility/FakeAddressUtility.java
+++ src/test/java/com/utility/FakeAddressUtility.java
@@ -7,9 +7,7 @@ import com.ui.pojo.AddressPOJO;
 
 public class FakeAddressUtility {
 
-	public static void main(String[] args) {
-		getFakeAddress();
-	}
+	
 
 	public static AddressPOJO getFakeAddress() {
 		Faker faker = new Faker(Locale.US);
